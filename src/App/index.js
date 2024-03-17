294cf1cabbe2f17b963d95905c652389d1706876
Jorge Leon <crashe24@hotmail.com>
2024-03-17 10:45:59 -0500

    first commit part 1

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,60 @@
 
+import { useState } from 'react';
 import './App.css';
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContex';
 
 
-function App() {
+function App () {
+  const [state, setstate] = useState('estado compartido');
+  return (
+    <>
+      <TodoHeader >
+        <TodoCounter />
+        <TodoSearch />
+       </TodoHeader> 
+      <TodoList >
+        <TodoItem state ={state}/>
+      </TodoList>
+    </>
+  )
+}
+
+function TodoHeader ({children}) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+function TodoList ({children}) {
+  return (
+    <>
+      <section className='TodoList-container'>
+        {children}
+
+      </section>
+    </>
+  )
+}
+
+function TodoCounter() {
+  return (<p>TodoCounter</p>)
+}
+function TodoSearch() {
+  return (<p>TodoSearch</p>)
+}
+function TodoItem({state}) {
+  return (<p>TodoItem : {state}</p>)
+}
+
+// function App() {
 
   
-  return (
-     <TodoProvider>
-       <AppUI />
-     </TodoProvider>
-    );
-}
+//   return (
+//      <TodoProvider>
+//        <AppUI />
+//      </TodoProvider>
+//     );
+// }
 
 export default App;
