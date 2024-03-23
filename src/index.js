51a6ecaec45ce5a3a2130655a6e17fae462f3594
Jorge Leon <crashe24@hotmail.com>
2024-03-23 06:32:50 -0500

    first change hoc

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// const App = (props) => {
+//     return (
+//     <h1>Hola {props.saludo}, {props.nombre}</h1>
+
+//     )
+// }
+
+// const withSaludo = (WrapperComponent) => {
+//     return function WrapperComponentWithSaludo(saludo) {
+//         return function ComponenteDeVerdad(props) {
+//             return (<>
+//                 <WrapperComponent {...props} saludo= {saludo}/>
+//                 <p>Estamos acompaniando al wrapper</p>
+//             </>)
+//         }
+        
+//     }
+// }
+
+//const AppWithFunctionHOC = withSaludo(App)('Buenas')
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+//root.render( <AppWithFunctionHOC  nombre= 'jorge' />);
+root.render( <App />);
+
